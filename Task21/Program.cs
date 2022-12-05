a432ac94caf48aa812ef0b596891b1e496922bfb
Poloshak <e.l.fomenko@yandex.ru>
2022-12-05 21:36:05 +0300

    new tasks

---
+++ Task21/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+//A (3,6); B (2,1) -> 5,09 
+//A (7,-5); B (1,-1) -> 7,21
+
+Console.Clear();
+Console.WriteLine("Введите x1 ");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y1 ");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите x2 ");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y2 ");
+double y2 = double.Parse(Console.ReadLine());
+double xdiff = x1 - x2;
+double ydiff = y1 - y2;
+double xdiff2 = Math.Pow(xdiff, 2);
+double ydiff2 = Math.Pow(ydiff, 2); 
+double d = Math.Sqrt(xdiff2 + ydiff2);
+Console.WriteLine($"d={d:f2}");
---
+++ Task_17/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+//причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+Console.Clear();
+Console.WriteLine("Введите координаты x ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты y ");
+int y = int.Parse(Console.ReadLine());
+if(x>0 && y>0)
+{
+    Console.WriteLine ("Первая четверть плоскости");
+}
+if(x<0 && y>0)
+{
+    Console.WriteLine ("Вторая четверть плоскости");
+}
+if(x<0 && y<0)
+{
+    Console.WriteLine ("Третья четверть плоскости");
+}
+if(x>0 && y<0)
+{
+    Console.WriteLine ("Четвертая четверть плоскости");
+}
+if (x==0 || y==0)
+{
+    Console.WriteLine ("На оси");
+}
